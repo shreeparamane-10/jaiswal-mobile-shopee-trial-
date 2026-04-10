0ad2a5490843fc33b231e24d9fde1e508880ac38
shreeparamane-10 <shreeparamane@gmail.com>
2026-04-10 23:11:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Welcome to Jaiswal Mobile Shopee Website");
+function searchProduct(){
+
+let input = document.getElementById("search").value.toLowerCase();
+
+let cards = document.querySelectorAll(".product-card");
+
+cards.forEach(card => {
+
+let name = card.innerText.toLowerCase();
+
+if(name.includes(input)){
+card.style.display="block"
+}
+else{
+card.style.display="none"
+}
+
+})
+
+}
+
+function aiAssistant(){
+alert("Hello 👋 I am the Jaiswal Mobile AI assistant. How can I help you?");
+}
